e676b33e07e31e6d971ee295627b8430f85c9e8a
DESKTOP-I3USAS7\PC <champoyradoo@gmail.com>
2026-04-28 12:31:40 +0800

    fix: unverified search

--- app/Http/Controllers/Admin/ListingController.php
+++ app/Http/Controllers/Admin/ListingController.php
@@ -417,7 +417,7 @@ class ListingController extends Controller
                     ->orderBy('created_at', 'asc')
                     ->paginate(10)
                 : Listing::search($q)
-                    ->where('is_verified', '0')
+                    ->where('is_verified', false)
                     ->paginate(10);
         }
 
