720fb496061d05db0a4e96ba84ac8ce8e8af0529
DESKTOP-I3USAS7\PC <champoyradoo@gmail.com>
2026-05-06 02:01:33 +0800

    feat: robots.txt

---
+++ app/Http/Controllers/RobotsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class RobotsController extends Controller
+{
+    public function index()
+    {
+        $body = "User-agent: *\n"
+            . "Disallow: /auth/login\n"
+            . "Disallow: /forgot-password\n"
+            . "Disallow: /reset-password/\n"
+            . "\n"
+            . 'Sitemap: ' . url('sitemap.xml') . "\n";
+
+        return response($body, 200, ['Content-Type' => 'text/plain']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\InquiryController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\PasswordController;
+use App\Http\Controllers\RobotsController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SitemapController;
 use Illuminate\Support\Facades\Route;
@@ -45,3 +46,4 @@ Route::post('/inquiries', [InquiryController::class, 'store'])
     ->name('inquiries.store');
 
 Route::get('/sitemap.xml', [SitemapController::class, 'index'])->name('sitemap');
+Route::get('/robots.txt', [RobotsController::class, 'index'])->name('robots');
