491de4c7c09b079287337709feca54d1bc20635c
DESKTOP-I3USAS7\PC <champoyradoo@gmail.com>
2026-05-07 01:39:12 +0800

    feat: api logout

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Auth\FieldLoginController;
+use App\Http\Controllers\Api\V1\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->name('v1.')->group(function () {
     Route::post('auth/field-login', FieldLoginController::class)
         ->name('auth.field-login');
+
+    Route::post('auth/logout', LogoutController::class)
+        ->middleware('auth:sanctum')
+        ->name('auth.logout');
 });
